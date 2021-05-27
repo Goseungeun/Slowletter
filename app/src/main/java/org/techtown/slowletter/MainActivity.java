a29f077ad1a3d3f14ae1a83ce3cdbce9e464f49f
irene <ireneee145@gmail.com>
2021-05-28 02:52:40 +0900

    login interface

--- app/src/main/java/org/techtown/slowletter/MainActivity.java
+++ app/src/main/java/org/techtown/slowletter/MainActivity.java
@@ -1,8 +1,12 @@
 package org.techtown.slowletter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2=findViewById(R.id.inbox_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
