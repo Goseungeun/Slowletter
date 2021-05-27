a003d02703b3ad923d08418a4f60b48b06f01a18
irene <ireneee145@gmail.com>
2021-05-28 04:04:12 +0900

    xml modify

--- app/src/main/java/org/techtown/slowletter/MainActivity.java
+++ app/src/main/java/org/techtown/slowletter/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),InboxList.class);
+                Intent intent=new Intent(getApplicationContext(),WriteLetter.class);
                 startActivity(intent);
             }
         });
